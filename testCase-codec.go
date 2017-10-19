4e61586d0de11d4d2fc528d4c990adde2770d5d6
Gotterdemarung <gotterdemarung@gmail.com>
2017-10-19 16:33:51 +0300

    correct int64 parsing

--- testCase-codec.go
+++ testCase-codec.go
@@ -3,6 +3,7 @@ package oscar
 import (
 	"encoding/base64"
 	"encoding/binary"
+	"github.com/mono83/oscar/util"
 	"github.com/yuin/gopher-lua"
 )
 
@@ -22,7 +23,12 @@ func lTestCasePackInt64Base64(L *lua.LState) int {
 		buf := make([]byte, (cnt-1)*8)
 		for i := 2; i <= cnt; i++ {
 			chunk := make([]byte, 8)
-			binary.BigEndian.PutUint64(chunk, uint64(L.ToInt64(i)))
+			i64, err := util.LuaToInt64(L, i)
+			if err != nil {
+				L.Error(lua.LString(err.Error()), 1)
+				return 0
+			}
+			binary.BigEndian.PutUint64(chunk, uint64(i64))
 			for j := 0; j < 8; j++ {
 				buf[(i-2)*8+j] = chunk[j]
 			}
---
+++ util/luaInt64.go
@@ -0,0 +1,12 @@
+package util
+
+import (
+	"github.com/yuin/gopher-lua"
+	"strconv"
+)
+
+// LuaToInt64 reads Lua function argument as string, and then parses it as int64
+func LuaToInt64(L *lua.LState, arg int) (int64, error) {
+	str := L.ToString(arg)
+	return strconv.ParseInt(str, 10, 64)
+}
